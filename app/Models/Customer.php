ff0465b661db8ba8a3f9994d5a098e1a539f0503
Maria <maria.shahla@darrebni.net>
2025-06-17 14:58:41 +0300

    to  enhance morph relation

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\HasAddresses;
 use App\Traits\HasContacts;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    use HasContacts;
+    use HasContacts,HasAddresses;
+    protected $fillable = ['name', 'email', 'phone', 'address'];
 
     public function user(){
-        return $this->morphTo(User::class, 'user');
+        return $this->morphOne(User::class, 'user');
     }
 }
