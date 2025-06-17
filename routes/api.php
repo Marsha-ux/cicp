43e0807d47c74d00750f22c1cda44cd785594489
Maria <maria.shahla@darrebni.net>
2025-06-17 16:02:21 +0300

    Fileprocess

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\FileController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     require __DIR__.'/admin.php';
@@ -15,5 +16,8 @@ Route::prefix('customer')->group(function () {
 
 Route::get('/test-api', function () {
     return response()->json(['message' => 'API Route Works!']);
+});
+Route::middleware('auth:sanctum')->group(function () {
+Route::post('/upload-file', [FileController::class, 'uploadFile']);
 });
 
